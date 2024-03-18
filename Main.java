3e099384cca73293091aade1a0641a1e5401c179
Michael-OConnor-03 <161645951+Michael-OConnor-03@users.noreply.github.com>
2024-03-18 14:58:32 -0500

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,9 @@
+import java.io.*;
+import utility.Utilites;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		Utilites u = new Utilites();
+		u.viewMainMenu();
+	}
+}
