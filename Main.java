ffc8429fec883ee3cef66b357e15cd3c5c2f69ca
Michael-OConnor-03 <161645951+Michael-OConnor-03@users.noreply.github.com>
2024-03-18 14:21:33 -0500

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,12 @@
+package pkg;
+import java.io.*;
+import utility.Utilites;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		Utilites u = new Utilites();
+		u.viewMainMenu();
+	}
+
+}
